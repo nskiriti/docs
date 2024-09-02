a335ba46bfd80bb31af60d8291ab33c867914632
Ravi <pagidipala.ravi@gmail.com>
2024-09-02 22:27:33 +0530

    class
    class

---
+++ js/class/class.js
@@ -0,0 +1,16 @@
+console.log("This is class.js start");
+function main() {
+    const ravi = new Employee(101, "ravi.kumar@gmail.com", 9916286789);
+    console.log("employee = " + ravi);
+    //const ravi = createEmployee(101, "ravi.kumar@gmail.com", 9916286789);
+
+    const empDetails1 = ravi.getEmployee();
+    console.log("read = " + empDetails1);
+
+    const empDetails2 = ravi.updateEmployee('ravi.p@gmail.com', 9876543210);
+    console.log("updated = " + empDetails2);
+
+    const empDetails3 = ravi.deleteEmployee();
+    console.log("delete = " + empDetails3);
+}
+console.log("This is class.js end");

3b7467d967857e57598d1d7c1cc2300a4ef97ac6
Ravi <pagidipala.ravi@gmail.com>
2025-01-04 12:42:37 +0530

    literals
    literals

---
+++ java/language_fundamentals/literals/Literals.java
@@ -0,0 +1,22 @@
+class Literals {
+    public static void main(String[] args) {
+        // Decimal
+        //int x = 10;
+        //System.out.println("x = "+x);
+        //Octal
+        //int oct = 010;
+        //System.out.println("oct = "+oct);
+        // Hexa
+        //int hex = 0x10;
+        //System.out.println("hex = "+hex);
+        //long
+        //int l1 = 10;
+        //int l2 = 10l; //CE : possible lossy conversion from long to int
+        //long l2 = 10l;
+        //System.out.println("l2 = "+l2);
+        //byte
+        byte b1 = 128;
+        System.out.println("b1 = "+b1);
+
+    }
+}

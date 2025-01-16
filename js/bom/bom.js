f59197c32b41801e291f2785df7c0d902b819bf0
Ravi <pagidipala.ravi@gmail.com>
2025-01-16 18:13:56 +0530

    dom and bom
    dom and bom

---
+++ js/bom/bom.js
@@ -0,0 +1,10 @@
+console.log("window");
+
+//alert
+alert("This is an alert");
+//prompt
+let age = prompt('How old are you?', 100);
+alert(`You are ${age} years old!`); // You are 100 years old!
+//confirm
+let isManager = confirm("Are you Manager?");
+alert( isManager ); // true if OK is pressed
